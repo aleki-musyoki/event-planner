9387dc944f1ad0cc870a1abfa72cb29f1fc635ba
aleki-musyoki <amusyoki.gm@gmail.com>
2020-06-07 16:49:57 +0300

    added calendarView

---
+++ app/src/main/java/com/enigma/eventplanner/Calendar.java
@@ -0,0 +1,16 @@
+package com.enigma.eventplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class Calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
